34404df43926bc91b61bc44c4ef2ff26a25e3a8c
Ryan DECIAN <ryan.decian.propgmail.com>
2024-12-18 23:18:45 +0100

    Test4 Script sh

--- git-commit.sh
+++ git-commit.sh
@@ -78,3 +78,4 @@ branch=$(git rev-parse --abbrev-ref HEAD)
 # Pousse sur la branche courante
 git push origin "$branch" || { echo "❌ Erreur : Push échoué."; exit 1; }
 echo "✅ Commit réussi, envoi sur la branche '$branch'..."
+
