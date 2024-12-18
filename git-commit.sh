780a8be8ba8df00e68072dc049471f83dbc0add3
Ryan DECIAN <ryan.decian.propgmail.com>
2024-12-18 23:46:10 +0100

    Test9 Script sh qui fonctionne enfin

--- git-commit.sh
+++ git-commit.sh
@@ -75,3 +75,4 @@ branch=$(git rev-parse --abbrev-ref HEAD)
 # Pousse sur la branche courante
 git push origin "$branch" || { echo "❌ Erreur : Push échoué."; exit 1; }
 echo "✅ Commit réussi, envoi sur la branche '$branch'..."
+
