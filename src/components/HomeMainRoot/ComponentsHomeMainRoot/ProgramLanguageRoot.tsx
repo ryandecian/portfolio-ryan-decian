9ddfc1d2136d088f3705302f712fedd916f60390
Ryan DECIAN <ryan.decian.propgmail.com>
2024-12-26 11:29:21 +0100

    Ajout d'une balise div intermediaire pour appliquer un style linear. La structure n'est pas terminé, ceci est un test

--- src/components/HomeMainRoot/ComponentsHomeMainRoot/ProgramLanguageRoot.tsx
+++ src/components/HomeMainRoot/ComponentsHomeMainRoot/ProgramLanguageRoot.tsx
@@ -1,15 +1,24 @@
 import style from "./ProgramLanguageRoot.module.css"
 
+/*Import des images*/
+import LogoReact from "../../../assets/ProgramLanguageImg/LogoReact.png"
+
 function ProgramLanguageRoot() {
     return (
          <div className={style.LanguageDevPC}>
-             <div className={style.div1}><p>test1</p></div>
-             <div className={style.div2}><p>test2</p></div>
-             <div className={style.div3}><p>test3</p></div>
-             <div className={style.div4}><p>test4</p></div>
-             <div className={style.div5}><p>test5</p></div>
-             <div className={style.div6}><p>test6</p></div>
-             <div className={style.div7}><p>test7</p></div>
+
+             <div className={style.Container1}>
+                 <div className={style.BorderContainer}>
+                     <img src={LogoReact} alt="Logo React" />
+                 </div>
+             </div>
+
+             <div className={style.Container2}><p>test2</p></div>
+             <div className={style.Container3}><p>test3</p></div>
+             <div className={style.Container4}><p>test4</p></div>
+             <div className={style.Container5}><p>test5</p></div>
+             <div className={style.Container6}><p>test6</p></div>
+             <div className={style.Container7}><p>test7</p></div>
          </div>
     )
 }
