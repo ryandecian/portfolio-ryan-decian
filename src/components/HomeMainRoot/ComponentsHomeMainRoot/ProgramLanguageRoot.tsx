7f7540000b7716d5a33bc7e30e2f0cefcb797079
Ryan DECIAN <ryan.decian.propgmail.com>
2024-12-26 17:31:42 +0100

    Ajout des Logos React, JS, CSS, module CSS, Node [C[C[CJS et HTML. Il manque des Logos

--- src/components/HomeMainRoot/ComponentsHomeMainRoot/ProgramLanguageRoot.tsx
+++ src/components/HomeMainRoot/ComponentsHomeMainRoot/ProgramLanguageRoot.tsx
@@ -2,23 +2,59 @@ import style from "./ProgramLanguageRoot.module.css"
 
 /*Import des images*/
 import LogoReact from "../../../assets/ProgramLanguageImg/LogoReact.png"
+import LogoNodeJS from "../../../assets/ProgramLanguageImg/LogoNodeJS.png"
+import LogoHTML from "../../../assets/ProgramLanguageImg/LogoHTMLWhite.png"
+import LogoCSS from "../../../assets/ProgramLanguageImg/LogoCSSWhite.png"
+import LogoJavaScript from "../../../assets/ProgramLanguageImg/LogoJavaScriptWhite.png"
+import LogoModuleCSS from "../../../assets/ProgramLanguageImg/LogoModuleCSS.png"
 
 function ProgramLanguageRoot() {
     return (
          <div className={style.LanguageDevPC}>
 
-             <div className={style.Container1}>
-                 <div className={style.BorderContainer}>
+             <div className={style.Container1PC}>
+                 <div className={style.BorderContainerPC}>
                      <img src={LogoReact} alt="Logo React" />
                  </div>
              </div>
 
-             <div className={style.Container2}><p>test2</p></div>
-             <div className={style.Container3}><p>test3</p></div>
-             <div className={style.Container4}><p>test4</p></div>
-             <div className={style.Container5}><p>test5</p></div>
-             <div className={style.Container6}><p>test6</p></div>
-             <div className={style.Container7}><p>test7</p></div>
+             <div className={style.Container2PC}>
+                 <div className={style.BorderContainerPC}>
+                     <img src={LogoCSS} alt="Logo CSS" />
+                     <h3></h3>
+                 </div>
+             </div>
+
+             <div className={style.Container3PC}>
+                 <div className={style.BorderContainerPC}>
+                     <img src={LogoModuleCSS} alt="Logo Module CSS" />
+                 </div>
+             </div>
+
+             <div className={style.Container4PC}>
+                 <div className={style.BorderContainerPC}>
+                     <img src={LogoHTML} alt="Logo HTML" />
+                 </div>
+             </div>
+
+             <div className={style.Container5PC}>
+                 <div className={style.BorderContainerPC}>
+                     <img src={LogoJavaScript} alt="Logo JavaScript" />
+                 </div>
+             </div>
+
+             <div className={style.Container6PC}>
+                 <div className={style.BorderContainerPC}>
+                     <img src={LogoNodeJS} alt="Logo NodeJS" />
+                 </div>
+             </div>
+
+             <div className={style.Container7PC}>
+                 <div className={style.BorderContainerPC}>
+                     <img src={LogoNodeJS} alt="Logo React" />
+                 </div>
+             </div>
+
          </div>
     )
 }
