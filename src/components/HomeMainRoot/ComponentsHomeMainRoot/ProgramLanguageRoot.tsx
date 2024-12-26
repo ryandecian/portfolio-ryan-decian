f9acc51eb3959c75fb203b1a216a06b620d9ef6e
Ryan DECIAN <ryan.decian.propgmail.com>
2024-12-26 11:01:22 +0100

    Transfère des logique CSS du composant HomeMainPC.css vers ProgramLanguageRoot.module.css stylisant ainsi le composant ProgramLanguageRoot.tsx de CSS en Module CSS

--- src/components/HomeMainRoot/ComponentsHomeMainRoot/ProgramLanguageRoot.tsx
+++ src/components/HomeMainRoot/ComponentsHomeMainRoot/ProgramLanguageRoot.tsx
@@ -1,15 +1,15 @@
-
+import style from "./ProgramLanguageRoot.module.css"
 
 function ProgramLanguageRoot() {
     return (
-         <div className="LanguageDevPC">
-             <div className="div1"><p>test1</p></div>
-             <div className="div2"><p>test2</p></div>
-             <div className="div3"><p>test3</p></div>
-             <div className="div4"><p>test4</p></div>
-             <div className="div5"><p>test5</p></div>
-             <div className="div6"><p>test6</p></div>
-             <div className="div7"><p>test7</p></div>
+         <div className={style.LanguageDevPC}>
+             <div className={style.div1}><p>test1</p></div>
+             <div className={style.div2}><p>test2</p></div>
+             <div className={style.div3}><p>test3</p></div>
+             <div className={style.div4}><p>test4</p></div>
+             <div className={style.div5}><p>test5</p></div>
+             <div className={style.div6}><p>test6</p></div>
+             <div className={style.div7}><p>test7</p></div>
          </div>
     )
 }
