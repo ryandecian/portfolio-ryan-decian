32ddfe4d9afa22bc54e80e22641bc6ed8e6f256f
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-01-09 23:16:19 +0100

    Etape 2 : Fournir le context

--- src/contexts/DataContexte.tsx
+++ src/contexts/DataContexte.tsx
@@ -1,3 +1,22 @@
 import { createContext } from "react";
+import { useState } from "react";
 
-const DataContexte = createContext(null)
+/* Création du contexte et nom du contexte*/
+const DataContext = createContext<string>("");
+
+/* Type de children. Cela type permet de prendre tout ce que React peux prendre :*/
+/* Elément JSX, string, number, null, un tableau d'élément, un fragment ou fragment*/
+type childrenType = {
+    children: React.ReactNode;
+}
+
+/*Mise a disposition du contexte*/
+export function DataProvider({children}: childrenType) {
+    const [data, setData] = useState<string>("")
+    return (
+        <DataContext.Provider
+             value={{date, setData}}>
+             {children}
+        </DataContext.Provider>
+    )
+}
