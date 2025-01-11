515061d7190a80309a990d5b8d1eb780624da04d
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-01-11 09:40:39 +0100

    Ajout de la logique du composant HomeMainPC dans HomeMainMobile

--- src/components/HomeMainRoot/HomeMainMobile/HomeMainMobile.tsx
+++ src/components/HomeMainRoot/HomeMainMobile/HomeMainMobile.tsx
@@ -2,7 +2,85 @@
 
 function HomeMainMobile() {
     return (
-        <section className="HomeMainMobile"></section>
+         <section className="HomeMainPC">
+
+             <section className="ContainerAboutHomeMainPC">
+
+                 <h2>A Propos</h2>
+
+                 <section className="SubContainerAboutHomeMainPC">
+
+                     <div className="PresentationPC">
+
+                         <div className="AvatarPC">
+                             <img src={Avatar} alt="Ryan DECIAN" />
+                         </div>
+
+                         <div className="DescriptionPC">
+                             <p>
+                                 Pleinement engagé dans la philosophie 
+                                 de l’apprentissage tout au long de la vie, 
+                                 je suis un développeur full stack avec une profonde passion 
+                                 pour JavaScript, React et tout ce qui concerne le développement 
+                                 Web. La combinaison unique de créativité, de logique, de 
+                                 technologie et de ne jamais être à court de nouvelles choses à 
+                                 découvrir, alimente mon enthousiasme et ma passion pour le 
+                                 développement Web. Quand je ne suis pas devant mon ordinateur, 
+                                 j’aime passer mon temps à lire, à me maintenir en forme et à faire de l'équitation.
+                             </p>
+                         </div>
+
+                     </div>
+
+
+                     <ProgramLanguageRoot />
+           
+                 </section>
+
+             </section>
+
+             <section className="ContainerProjetHomeMainPC">
+                 <h2>Projets</h2>
+                 <section className="MapProjetPC">
+                     {
+                         data.map((Var) => {
+                              return (
+                                 <CardProjetRoot
+                                     moduleContainerCardProjetRoot = {style.ContainerCardProjetRootPC}
+                                     moduleCardProjetImg = {style.CardProjetImgPC}
+                                     moduleContainerCardProjetInfo = {style.ContainerCardProjetInfoPC}
+                                     moduleTitleCardProjet = {style.TitleCardProjetPC}
+                                     moduleYearCardProjet = {style.YearCardProjetPC}
+                                     moduleButtonCardProjet = {style.ButtonCardProjetPC}
+                                     src = {Var.img}
+                                     title = {Var.title}
+                                     yearStart = {Var.date.yearStart}
+                                     yearEnd = {Var.date.yearEnd}
+                                     key = {Var.id}
+
+                                     moduleContainerSubCardProjetRoot = {style.ContainerSubCardProjetRootPC}
+                                     moduleSubContainerSubCardProjetRoot = {style.SubContainerSubCardProjetRootPC}
+                                     moduleLienProdTitle = {style.LienProdTitlePC}
+                                     moduleTitleSubCardProjet = {style.TitleSubCardProjetPC}
+                                     moduleDateSubCardProjet = {style.DateSubCardProjetPC}
+                                     moduleContainerSubCardProjetInfo = {style.ContainerSubCardProjetInfoPC}
+                                     moduleDescriptionSubCardProjet = {style.DescriptionSubCardProjetPC}
+                                     description1 = {Var.description1}
+                                     description2 = {Var.description2}
+                                     description3 = {Var.description3}
+                                     moduleImgLangageDev = {style.ImgLangageDevPC}
+                                     languageDev = {Var.languageDev || []}
+                                     lienProd = {Var.lienProd}
+                                     moduleLienProd = {style.LienProdPC}
+                                 />
+                             )
+                         })
+                     }
+                 </section>
+                 <Link to="Projets" className="ButtonHomeMainPC">Voir plus de projets</Link>
+             </section>
+
+         </section>
     )
 }
 
