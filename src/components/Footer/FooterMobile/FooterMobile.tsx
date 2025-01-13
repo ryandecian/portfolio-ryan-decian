2e182b9b5a3aae80495d3dc5816c2a50dd8682b4
Ryan DECIAN <ryan.decian.propgmail.com>
2025-01-13 14:28:55 +0100

    Création du dossier FooterMobile et des fichiers FooterMobile.tsx et .css + ajout de la logique basé sur FooterPC.tsx et .css

---
+++ src/components/Footer/FooterMobile/FooterMobile.tsx
@@ -0,0 +1,18 @@
+import "./FooterMobile.css"
+
+function FooterMobile() {
+    const dateNow = new Date().getFullYear();
+
+    return (
+        <section className="FooterMobile">
+         <p className="CopyrightMobile"> 
+                 <span className="DateLogMobile">
+                     {dateNow === 2024 ? `©2024 ` : `©2024 - ${dateNow} `}
+                 </span>
+             Ryan DECIAN. Tous droits réservés.
+         </p>
+        </section>
+    )
+}
+
+export default FooterMobile
