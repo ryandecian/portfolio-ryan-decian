d8230981b02b01d97459816acced37df8b27af87
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-01-15 19:28:00 +0100

    Ajout du routage pour Netlify

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -5,18 +5,40 @@ import FooterRoot from "../components/Footer/FooterRoot";
 import { Helmet } from "react-helmet";
 
 function HomePage() {
+     const SEO = {
+           title: "Accueil - Portfolio Ryan DECIAN",
+           description: "Développeur web full stack junior",
+           url: "https://portfolio-ryan-decian.netlify.app",
+     }
+
     return(
-        <Helmet>
-        <header>
-             <NavRoot />
-        </header>
-        <main>
-             <HomeMainRoot />
-        </main>
-        <footer>
-             <FooterRoot />
-        </footer>
-        </Helmet>
+           <Helmet>
+                {/* Balises SEO */}
+                     {/* Balise pour Google */}
+                          <title>{SEO.title}</title>
+                          <meta name="description" content={SEO.description}/>
+
+                     {/* Balises OpenGraph - Facebook, Linkedin, WhatsApp, Instagram*/}
+                          <meta property="og:title" content={SEO.title} />
+                          <meta property="og:description" content={SEO.description} />
+                          <meta property="og:image" content="" /> {/* Non fournis pour le moment */}
+                          <meta property="og:url" content={SEO.url} />
+
+                     {/* Balises Twitter Cards */}
+                          <meta name="twitter:title" content={SEO.title} />
+                          <meta name="twitter:description" content={SEO.description} />
+
+
+                <header>
+                     <NavRoot />
+                </header>
+                <main>
+                     <HomeMainRoot />
+                </main>
+                <footer>
+                     <FooterRoot />
+                </footer>
+           </Helmet>
     )
 }
 
