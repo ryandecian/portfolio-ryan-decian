0989bb4d7b75d33fbe6852f57860c7c020c88b70
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-01-21 13:25:38 +0100

    Changement des import de react-helmet-async dans HomePage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,7 +2,8 @@ import "./HomePage.css"
 import NavRoot from "../components/NavRoot/NavRoot";
 import HomeMainRoot from "../components/HomeMainRoot/HomeMainRoot";
 import FooterRoot from "../components/Footer/FooterRoot";
-import { Helmet } from "react-helmet";
+import { Helmet } from "react-helmet-async";
+
 
 function HomePage() {
      const SEO = {
